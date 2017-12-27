835d97112a1a213caa05f086d747e04e25a1ab8b
alackfeng <fengyue7576199@163.com>
2017-12-27 18:02:18 +0800

    taurus add ios build

--- index.ios.js
+++ index.ios.js
@@ -10,4 +10,4 @@ import {
 
 import ClientApp from './js/components/ClientApp';
 
-AppRegistry.registerComponent('UniversalNativeBoilerplate', () => ClientApp);
+AppRegistry.registerComponent('TarotApp', () => ClientApp);
